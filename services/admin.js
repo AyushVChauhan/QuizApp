793577f7faba9289eb2a242829349a62ef504fe5
Tanisha928 <tanishadave207@gmail.com>
2023-08-22 20:55:58 +0530

    admin

--- services/admin.js
+++ services/admin.js
@@ -1,3 +1,19 @@
+const adminServices = require("../services/admin");
+async function addDepartment(req, res) {
+    let data = await adminServices.newDepartment(req.body.name);
+   
+    // if(data!=null)
+    // {
+    //     let token = jwt.sign({username:req.body.username,password:req.body.password,role:0},process.env.JWT_SeCRET);
+    //     res.cookie("auth",token);
+    //     res.redirect("/student");
+    // }
+    
+    
+        res.redirect("/admin");
+
+}
+module.exports = {addDepartment};
 //Role=0 Teacher accept
 //Add subject,Delete,Edit
 //Department 
