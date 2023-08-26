c8a6be7dc21c0d5d249144ab05c1e086a1db63fd
Tanisha928 <tanishadave207@gmail.com>
2023-08-26 15:10:34 +0530

    teacher captcha

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -2,29 +2,41 @@ const teacherServices = require("../services/teacher");
 const jwt = require("jsonwebtoken");
 const xlsx = require("xlsx");
 async function login(req, res) {
-  let data = await teacherServices.loginFetch(
-    req.body.username,
-    req.body.password
-  );
-  if (data != null) {
-    let token = jwt.sign(
-      {
-        username: req.body.username,
-        password: req.body.password,
-        role: data.role + 1,
-      },
-      process.env.JWT_SECRET
+  if(req.body.captcha === req.body.inputcaptcha){
+    let data = await teacherServices.loginFetch(
+      req.body.username,
+       req.body.password
     );
-    res.cookie("auth", token);
-    if (data.role === 0) {
-      console.log("login");
-      res.redirect("/teacher");
+    if (data != null) {
+     
+      // res.redirect("/teacher");
+    
+            let token = jwt.sign(
+        {
+          username: req.body.username,
+           password: req.body.password,
+          role: data.role + 1,
+        },
+        process.env.JWT_SECRET
+      );
+      res.cookie("auth", token);
+      if (data.role === 0) {
+        console.log("login");
+        res.redirect("/teacher");
+      } else {
+        res.redirect("/admin");
+      }
     } else {
-      res.redirect("/admin");
+      res.redirect("/teacher/login");
     }
-  } else {
+    
+  }
+  else{
     res.redirect("/teacher/login");
   }
+ 
+ 
+  
 }
 function loginGet(req, res) {
   res.render("teacher/teacher_login");
