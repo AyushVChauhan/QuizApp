48d44f1579195dd7ccd7601b399795eb2fa1bc0e
AyushVChauhan <ayush2004chauhan2526@gmail.com>
2023-09-02 10:41:20 +0530

    question with files

--- routes/teacher.js
+++ routes/teacher.js
@@ -6,12 +6,19 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const myCache = require("../controllers/cache");
 const storage = multer.diskStorage({
-  destination: "./public/files", filename: (req, file, cb) => {
-    cb(null, "schema.xlsx")
-  }
-})
+    destination: "./public/files",
+    filename: (req, file, cb) => {
+        cb(null, "schema.xlsx");
+    },
+});
 const upload = multer({ storage: storage });
-
+const questionStorage = multer.diskStorage({
+    destination: "./public/files/questions",
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + ".jpg");
+    },
+});
+const questionUpload = multer({ storage: questionStorage });
 router.post("/login", teacherControllers.login);
 router.get("/login", teacherControllers.loginGet);
 
@@ -19,11 +26,25 @@ router.use(middleware);
 
 router.get("/", teacherControllers.teacherDashboard);
 router.post("/addTopic", teacherControllers.addTopic);
-router.post("/addGroup", upload.single('excel') , teacherControllers.addGroup);
+router.post("/addGroup", upload.single("excel"), teacherControllers.addGroup);
 router.post("/viewGroup", teacherControllers.viewGroup);
 router.get("/addQuestion/topics", teacherControllers.addQue_subSelect);
-router.get("/addQuestion/question",questionMiddleware, teacherControllers.addQue_question);
-router.post("/addQuestion/question", questionMiddleware, teacherControllers.addQuestion);
+router.get(
+    "/addQuestion/question",
+    questionMiddleware,
+    teacherControllers.addQue_question
+);
+router.post(
+    "/addQuestion/question",
+    questionMiddleware,
+    teacherControllers.addQuestion
+);
+router.post(
+    "/addQuestion/question/files",
+    questionMiddleware,
+    questionUpload.array('questionFiles'),
+    teacherControllers.addQuestionFiles
+);
 router.post("/addQuestion/getTopics", teacherControllers.getTopics);
 router.post("/addQuestion/setTopics", teacherControllers.setTopics);
 router.get("/addQuiz/getGroups", teacherControllers.getGroups);
@@ -32,33 +53,28 @@ router.post("/addQuiz/setQuiz", teacherControllers.setQuiz);
 router.get("/addQuiz/setQuiz", teacherControllers.createQuiz);
 // router.post("/addQuestion/topics", teacherControllers.addQue_subSelect);
 
-function questionMiddleware(req,res,next){
-  if(req.session.topics)
-  {
-    next();
-  }
-  else
-  {
-    res.redirect("/teacher/addQuestion/topics");
-  }
+function questionMiddleware(req, res, next) {
+    if (req.session.topics) {
+        next();
+    } else {
+        res.redirect("/teacher/addQuestion/topics");
+    }
 }
 
 function middleware(req, res, next) {
-  let cookie = req.cookies.auth;
-  if (cookie) {
-    let data = jwt.verify(cookie, process.env.JWT_SECRET);
-    console.log
-    if (data.role === 1) {
-      //console.log("Next");
-      next();
+    let cookie = req.cookies.auth;
+    if (cookie) {
+        let data = jwt.verify(cookie, process.env.JWT_SECRET);
+        console.log;
+        if (data.role === 1) {
+            //console.log("Next");
+            next();
+        } else {
+            res.redirect("/teacher/login");
+        }
+    } else {
+        res.redirect("/teacher/login");
     }
-    else {
-      res.redirect("/teacher/login");
-    }
-  }
-  else {
-    res.redirect("/teacher/login");
-  }
 }
 
 module.exports = router;
